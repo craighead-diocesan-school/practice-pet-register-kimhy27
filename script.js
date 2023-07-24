bd4fc426b1476009e097fd816f068d48d1730917
Hyunji <124632128+kimhy27@users.noreply.github.com>
2023-07-24 22:51:08 +0000

    name complete

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let NameArray = [
-    'Kiko',
-    'Felix',
-    'Bubba',
-    'Winky',
-    'Layla',
-    'Kkami',
-    'Berry'
+    'Kiko',' Felix',' Layla',' Kkami',' Berry'
 ]
 
 function AddName() {
-    
+    // lets user input the name of the pet
+    let name = prompt("What is the name of the pet?")
+    // add a new name to add to array of names
+    NameArray.push(name)
 }
+
+function names() {
+    //lets shows user names of all the pets
+    alert(NameArray)
+}
+
+let SpeciesArray
+
 
